ca330ebcc43f925cd8785f7dea4b8fd5ca7022f2
dannybeckett <daniel.beckett@gmail.com>
2014-01-04 22:11:28 +0000

    Update weather.js

--- source/plugins/weather.js
+++ source/plugins/weather.js
@@ -1,29 +1,31 @@
 (function () {
-"use strict";
 
-function command ( args, cb ) {
-	IO.jsonp({
-		url : 'http://dannybeckett.co.uk/AviationBot/Weather.php',
-		jsonpName : 'callback',
-		data : {
-			a : args.toString()
+	function command ( args, cb ) {
+		args.reply('Working');
+		
+		IO.jsonp({
+			url : 'http://dannybeckett.co.uk/AviationBot/Weather.php',
+			jsonpName : 'callback',
+			data : {
+				a : args.toString()
+			},
+			fun : finish
+		});
+	
+		function finish ( resp ) {
+			args.reply( resp/*.data.METAR.raw_text*/ );
+		}
+	}
+	
+	bot.addCommand({
+		name : 'weather',
+		fun : command,
+		permissions : {
+			del : 'NONE'
 		},
-		fun : finish
+	
+		description : 'testing',
+		async : true
 	});
 
-	function finish ( resp ) {
-		args.reply( resp.data.METAR.raw_text );
-	}
-}
-
-bot.addCommand({
-	name : 'weather',
-	fun : command,
-	permissions : {
-		del : 'NONE'
-	},
-
-	description : 'testing',
-	async : true
-});
 })();
