44161992ea142cdf2f10d3b6fa79732b853c4c60
dannybeckett <daniel.beckett@gmail.com>
2014-01-04 22:08:17 +0000

    Update weather.js

--- source/plugins/weather.js
+++ source/plugins/weather.js
@@ -1,69 +1,29 @@
 (function () {
 "use strict";
 
-var weather = {
-	city : function ( city, cb ) {
-		IO.jsonp({
-			url : 'http://dannybeckett.co.uk/AviationBot/Weather.php',
-			jsonpName : 'callback',
-			data : {
-				a : city
-			},
+function command ( args, cb ) {
+	IO.jsonp({
+		url : 'http://dannybeckett.co.uk/AviationBot/Weather.php',
+		jsonpName : 'callback',
+		data : {
+			a : args.toString()
+		},
+		fun : finish
+	});
 
-			fun : this.finishCb( cb ),
-			error : this.errorCb( cb )
-		});
-	},
-
-	finishCb : function ( cb ) {
-		var self = this;
-
-		return function ( resp ) {
-			cb( self.format(resp) );
-		};
-	},
-	errorCb : function ( cb ) {
-		return cb;
-	},
-
-	format : function ( resp ) {
-		var main = resp.main;
-
-		if ( !main ) {
-			console.error( resp );
-			return 'Sorry, I couldn\'t get the data: ' + resp.message;
-		}
-
-		return this.formatter( resp );
-	},
-	formatter : function ( data ) {
-		/*var ret = bot.adapter.link(
-				data.name, 'http://openweathermap.org/city/' + data.id
-			) + ': ';
-
-		return ret;*/
-		return data.data.METAR.raw_text;
-	}
-};
-
-function weatherCommand ( args ) {
-	if ( args.content ) {
-		weather.city( args.content, args.reply.bind(args) );
-	}
-	else {
-		return 'See `/help weather` for usage info';
+	function finish ( resp ) {
+		args.reply( resp.data.METAR.raw_text );
 	}
 }
 
 bot.addCommand({
 	name : 'weather',
-	fun : weatherCommand,
+	fun : command,
 	permissions : {
 		del : 'NONE'
 	},
-	async : true,
 
-	description : 'Gets current weather: ' +
-		'`/weather ICAO`'
+	description : 'testing',
+	async : true
 });
-}());
+})();
