4c25e8a3e0d7651d163c5f98b957179af4573efd
dannybeckett <daniel.beckett@gmail.com>
2014-01-04 23:47:20 +0000

    Update weather.js

--- source/plugins/weather.js
+++ source/plugins/weather.js
@@ -1,30 +1,38 @@
-(function () {
+var weather = {
 
-	function command(args, cb)
+	descrip: 'Retrieves the METAR weather data for a specified ICAO airport - e.g. `!!weather PHTO`',
+	
+	command: function(args, cb)
 	{
+		var icao = args.toString();
+		
+		if(!icao)
+		{
+			args.directreply(descrip);
+		}
+		
 		IO.jsonp({
 			url:		'http://dannybeckett.co.uk/AviationBot/Weather.php',
 			jsonpName:	'callback',
 			fun:		finish,
 			data:		{
-						a : args.toString()
+						a:	icao
 					}
 		});
 	
 		function finish(resp)
 		{
-			args.reply(resp.data.METAR.raw_text);
+			args.directreply(resp.data.METAR.raw_text);
 		}
 	}
+};
 	
-	bot.addCommand({
-		name:		'weather',
-		fun:		command,
-		description:	'Retrieves the METAR weather data for a specified ICAO airport - e.g. !!weather PHTO',
-		async:		true,
-		permissions:	{
-					del:	'NONE'
-				}
-	});
-
-})();
+bot.addCommand({
+	name:		'weather',
+	fun:		weather.command,
+	description:	weather.descrip,
+	async:		true,
+	permissions:	{
+				del:	'NONE'
+			}
+});
