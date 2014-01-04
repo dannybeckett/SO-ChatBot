26c8cc59cfa6e7ed3626d3ada48f13ae47fabfbd
dannybeckett <daniel.beckett@gmail.com>
2014-01-04 22:44:17 +0000

    Update weather.js

--- source/plugins/weather.js
+++ source/plugins/weather.js
@@ -7,13 +7,13 @@
 			url : 'http://dannybeckett.co.uk/AviationBot/Weather.php',
 			jsonpName : 'callback',
 			data : {
-				a : 'PHTO' /*args.toString()*/
+				a : args.toString()
 			},
 			fun : finish
 		});
 	
 		function finish ( resp ) {
-			args.reply( 'ok' /*resp.data.METAR.raw_text*/ );
+			args.reply(resp.data.METAR.raw_text);
 		}
 	}
 	
