86cb69b0dd7c6f91b3f1409b4ecc677411228b49
dannybeckett <daniel.beckett@gmail.com>
2014-01-04 08:59:12 +0000

    Used as a template for getting aviation weather data/METARs - mostly deleted existing

--- source/plugins/weather.js
+++ source/plugins/weather.js
@@ -1,55 +1,17 @@
 (function () {
 "use strict";
 
-var fahrenheitCountries = Object.TruthMap([
-	//the API returns US in a variety of forms...
-	'US', 'United States of America', 'United States',
-	//other than the US, it's used in Belize, Bahamas and and Cayman Islands
-	'BZ', 'Belize', // http://www.hydromet.gov.bz/
-	'BS', 'Bahamas', // http://archive.is/RTD4
-	'KY', 'Cayam Islands' // http://www.weather.ky/forecast/index.htm
-]);
-
 var weather = {
-	latlon : function ( lat, lon, cb ) {
-		var nlat = Number( lat ),
-			nlon = Number( lon );
-
-		var errs = [];
-		if ( nlat < -180 || nlat > 180 ) {
-			errs.push( 'Latitude must be between -180 and 180' );
-		}
-		if ( nlon < -180 || nlon > 180 ) {
-			errs.push( 'Longitude must be between -180 and 180' );
-		}
-
-		if ( errs.length ) {
-			cb( errs.join('; ') );
-			return;
-		}
-
+	icao : function ( city, cb ) {
 		IO.jsonp({
-			url : 'http://api.openweathermap.org/data/2.5/weather',
+			url : 'http://aviationweather.gov/adds/dataserver_current/httpparam',
 			jsonpName : 'callback',
 			data : {
-				lat : lat,
-				lon : lon,
-				cnt : 1, //limit to 1 result
-				type : 'json'
-			},
-
-			fun : this.finishCb( cb ),
-			error : this.errorCb( cb )
-		});
-	},
-
-	city : function ( city, cb ) {
-		IO.jsonp({
-			url : 'http://api.openweathermap.org/data/2.5/weather',
-			jsonpName : 'callback',
-			data : {
-				q : city,
-				type : 'json'
+				dataSource:	'metars',
+				requestType:	'retrieve',
+				format:		'xml',
+				stationString:	icao,
+				hoursBeforeNow:	4
 			},
 
 			fun : this.finishCb( cb ),
@@ -64,6 +26,7 @@ var weather = {
 			cb( self.format(resp) );
 		};
 	},
+	
 	errorCb : function ( cb ) {
 		return cb;
 	},
@@ -78,50 +41,23 @@ var weather = {
 
 		return this.formatter( resp );
 	},
+	
 	formatter : function ( data ) {
-		var temps = data.main,
-			ret;
-
-		temps.celsius = ( temps.temp - 273.15 ).maxDecimal( 4 );
-
 		ret =
 			bot.adapter.link(
-				data.name, 'http://openweathermap.org/city/' + data.id
-			) + ': ';
-
-		//to help our dear American friends, also include fahrenheit
-		if ( fahrenheitCountries[data.sys.country] ) {
-			temps.fahrenheit = ( temps.temp * 9/5 - 459.67 ).maxDecimal( 4 );
-			ret += '{fahrenheit}F ({celsius}C, {temp}K)'.supplant( temps );
-		}
-		//and to those of us with one less insanity
-		else {
-			ret += '{celsius}C ({temp}K)'.supplant( temps );
-		}
-
-		var descs = ( data.weather || [] ).map(function ( w ) {
-			return w.description;
-		}).join( ', ' );
-
-		if ( descs ) {
-			ret += ', ' + descs;
-		}
+				data.name, 'http://aviationweather.gov/adds/metars/?station_ids=' + data.id + '&std_trans=translated&chk_metars=on&hoursStr=most+recent+only&submitmet=Submit'
+			) + ': ' + data.main;
 
 		return ret;
 	}
 };
 
-var latlon = /\((-?\d+\.?\d*),\s*(-?\d+\.?\d*)\)/;
 function weatherCommand ( args ) {
-	var parts = latlon.exec( args );
-	if ( parts ) {
-		weather.latlon( parts[1], parts[2], args.reply.bind(args) );
-	}
-	else if ( args.content ) {
-		weather.city( args.content, args.reply.bind(args) );
+	if ( args.content ) {
+		weather.icao( args.content, args.reply.bind(args) );
 	}
 	else {
-		return 'See `/help weather` for usage info';
+		return 'You must specify an ICAO airport name - e.g. `!!weather PHTO`';
 	}
 }
 
@@ -132,8 +68,7 @@ bot.addCommand({
 		del : 'NONE'
 	},
 	async : true,
-
-	description : 'Gets current weather: ' +
-		'`/weather (lan, lon)` or `/weather city`'
+	description : 'Gets current METAR weather data for a specified ICAO airport: ' +
+		'`/weather PHTO`'
 });
 }());
