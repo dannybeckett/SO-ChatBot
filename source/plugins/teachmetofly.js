78476e97f1c40228e05dfadf92a76afa16b5d51f
dannybeckett <daniel.beckett@gmail.com>
2014-01-13 03:45:15 +0000

    Update teachmetofly.js

--- source/plugins/teachmetofly.js
+++ source/plugins/teachmetofly.js
@@ -8,7 +8,10 @@ var teachmetofly = {
 		"Push forward: buildings get bigger. Pull back: buildings get smaller.",
 		"Aviate, Navigate, Communicate, in that order!",
 		"There are old pilots, and there are bold pilots, but there are no old bold pilots!",
-		"If a pilot screws up, the pilot dies; if ATC screws up, the pilot dies."
+		"If a pilot screws up, the pilot dies; if ATC screws up, the pilot dies.",
+		"Takeoffs are optional. Landings are mandatory.",
+		"To land: Airspeed, Centerline, PAPI *(repeat)*",
+		"The propeller is just a giant fan to keep the pilot cool; if it stops, the pilot starts sweating!",
 	],
 	
 	command: function(args, cb)
