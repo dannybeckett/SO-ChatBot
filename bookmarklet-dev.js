2615915fe125d7ef382dcdffeaa1d10a6da51c60
dannybeckett <daniel.beckett@gmail.com>
2014-01-18 23:09:43 +0000

    Delete bookmarklet-dev.js

--- bookmarklet-dev.js
+++
@@ -1,1 +0,0 @@
-(function(){var a=document.createElement("script");a.src="https://raw.github.com/Zirak/SO-ChatBot/master/master.js";a.onload=function(){bot.activateDevMode();};document.head.appendChild(a)})();
